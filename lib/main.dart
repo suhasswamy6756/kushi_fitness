96c86c8a171d0a1f3c4a4d9789bf40dc81c15154
Suhas181 <suhasswamy6756@gmail.com>
2024-05-02 17:59:55 +0530

    UI part done,stil some images

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,17 @@
 import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:kushi_3/pages/Fragments/mainFragments/group_fragment.dart';
-// import 'package:kushi/pages/Fragments/mainFragments/group_fragment.dart';
-// import 'package:kushi/pages/otp.dart';
-// import 'package:kushi/pages/selectGender.dart';
-// import 'package:kushi/pages/selectHeight.dart';
-// import 'package:kushi/pages/selectWeight.dart';
-// import 'package:kushi/pages/signin.dart';
-// import 'package:kushi/pages/signup.dart';
-// import 'package:kushi/pages/testingPages/stepTest.dart';
-// import 'package:kushi/service/auth/auth_gate.dart';
-// import 'package:kushi/themes/dark_mode.dart';
-// import 'package:kushi/themes/light_mode.dart';
-
-// import 'package:kushi_3/pages/contact_list.dart';
-import 'package:kushi_3/pages/introslider.dart';
+import 'package:kushi_3/pages/Fragments/mainFragments/home_fragment.dart';
 import 'package:kushi_3/pages/mainactivity.dart';
+
 import 'package:kushi_3/pages/otp.dart';
-// import 'package:kushi_3/pages/person_list.dart';
+import 'package:kushi_3/pages/refer_page.dart';
+import 'package:kushi_3/pages/referal_code.dart';
+
 import 'package:kushi_3/pages/selectGender.dart';
 import 'package:kushi_3/pages/selectHeight.dart';
 import 'package:kushi_3/pages/selectWeight.dart';
 import 'package:kushi_3/pages/signup.dart';
-import 'package:kushi_3/pages/testingPages/database.dart';
-import 'package:kushi_3/service/auth/auth_gate.dart';
 import 'package:kushi_3/service/auth/auth_service.dart';
 import 'package:provider/provider.dart';
 import 'firebase_options.dart';
@@ -31,9 +19,7 @@ import 'package:flutter/material.dart';
 import 'package:kushi_3/pages/signin.dart';
 import 'package:kushi_3/themes/dark_mode.dart';
 import 'package:kushi_3/themes/light_mode.dart';
-import 'package:kushi_3/model/globals.dart' as globals;
 import 'package:kushi_3/pages/testingPages/stepTest.dart';
-import 'package:kushi_3/pages/testingPages/fireStoreTest.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized(); // Ensures all plugins are initialized
@@ -41,6 +27,7 @@ Future<void> main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );// Asynchronously initialize Firebase
   await FirebaseAppCheck.instance.activate();
+
   runApp(
       MultiProvider(
           providers: [
@@ -74,9 +61,10 @@ class MyApp extends StatelessWidget {
           '/phoneVerification': (context) => SignIn(),
           '/userinfo': (context) => SignUp(),
           '/contactList':(context) => ContactList(),
+          '/referalpage': (context)=> ReferralScreen(),
         },
 
-      home:  MainActivity(namey: "suhas",),
+      home:  HomePage(),
     );
   }
 }
---
+++ lib/pages/refer_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ReferalPage extends StatefulWidget {
+  const ReferalPage({super.key});
+
+  @override
+  State<ReferalPage> createState() => _ReferalPageState();
+}
+
+class _ReferalPageState extends State<ReferalPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
