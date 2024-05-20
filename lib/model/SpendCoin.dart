f93bb825714ea2abd1e7f06cba2bdc4fe955de8b
SujalKThapa <136220535+SujalKThapa@users.noreply.github.com>
2024-05-20 21:04:09 +0530

    Redeem part completed
    Note: Testing has not been done.
    Nitya and Nisha, counting on you both.

--- lib/model/SpendCoin.dart
+++ lib/model/SpendCoin.dart
@@ -1,6 +1,7 @@
 library SpendCoin;
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'dart:developer' as dev;
 import 'globals.dart' as globals;
 
@@ -8,8 +9,11 @@ spendToken(int Price) async{
   FirebaseFirestore firestore = FirebaseFirestore.instance;
   CollectionReference coins = firestore.collection("40RupeeTokens");
   CollectionReference twentyCoins = firestore.collection("20RupeeToken");
+  CollectionReference halfCoins = firestore.collection("Half20");
+
   QuerySnapshot querySnapshot = await coins.where('UID', isEqualTo: FirebaseAuth.instance.currentUser!.uid.toString()).get();
-  QuerySnapshot twentyQuery = await coins.where('UID', isEqualTo: FirebaseAuth.instance.currentUser!.uid.toString()).get();
+  QuerySnapshot twentyQuery = await twentyCoins.where('UID', isEqualTo: FirebaseAuth.instance.currentUser!.uid.toString()).get();
+  QuerySnapshot halfQuery =  await coins.where('UID', isEqualTo: FirebaseAuth.instance.currentUser!.uid.toString()).get()
   if(Price >= 40){
     if(querySnapshot.docs.isNotEmpty){
       DocumentSnapshot firstDocument = querySnapshot.docs.first;
@@ -50,14 +54,45 @@ spendToken(int Price) async{
       dev.log("Deleted 40 Rupee Token");
       globals.generate20RupeeToken();
       dev.log("Generated 20 Rupee Token");
-      Price -= 40;
+      Price -= 20;
       if(Price != 0){
         spendToken(Price);
       }
     }
   }
   else if(Price >= 10){
-    // Subtract half coin here, note by Sujal for Nitya and Nisha.
-    // (Optional tip) Store half-coins in a seperate collection in Firebase. It would make the whole thing much easier.
+    if(halfQuery.docs.isNotEmpty){
+      DocumentSnapshot firstDocument = halfQuery.docs.first;
+      await coins.doc(firstDocument.id).delete();
+      dev.log("Deleted Half coin");
+      Price -= 10;
+      if(Price != 0){
+        spendToken(Price);
+      }
+    }
+    else if(twentyQuery.docs.isNotEmpty){
+      DocumentSnapshot firstDocument = twentyQuery.docs.first;
+      await coins.doc(firstDocument.id).delete();
+      dev.log("Deleted 20 Rupee Token");
+      globals.generateHalfCoin();
+      dev.log("Generated Half of a 20 Rupee Token");
+      Price -= 10;
+      if(Price != 0){
+        spendToken(Price);
+      }
+    }
+    else if(querySnapshot.docs.isNotEmpty){
+      DocumentSnapshot firstDocument = querySnapshot.docs.first;
+      await coins.doc(firstDocument.id).delete();
+      dev.log("Deleted 40 Rupee Token");
+      globals.generate20RupeeToken();
+      dev.log("Generated 20 Rupee Token");
+      globals.generateHalfCoin();
+      dev.log("Generated Half of a 20 Rupee Token");
+      Price -= 10;
+      if(Price != 0){
+        spendToken(Price);
+      }
+    }
   }
 }
--- lib/model/globals.dart
+++ lib/model/globals.dart
@@ -7,6 +7,8 @@ import 'dart:convert';
 import 'dart:developer' as developer;
 import 'dart:math';
 
+import 'package:kushi_3/model/SpendCoin.dart';
+
 dynamic userName = "Name";
 dynamic uid;
 var stepsToday = 0;
@@ -14,6 +16,41 @@ bool dailyToken = false;
 String date = "";
 var lastSteps = 0;
 var countedSteps = 0;
+
+redeemDiscount(int billValue){
+  if(billValue >= 1500){
+    spendToken(120);
+  }
+  else if(billValue >= 1001){
+    spendToken(120);
+  }
+
+  else if(billValue >= 800){
+    spendToken(100);
+  }
+  else if(billValue >= 600){
+    spendToken(80);
+  }
+  else if(billValue > 450){
+    spendToken(70);
+  }
+  else if(billValue > 400){
+    spendToken(60);
+  }
+  else if(billValue > 350){
+    spendToken(50);
+  }
+  else if(billValue > 300){
+    spendToken(40);
+  }
+  else if(billValue > 250){
+    spendToken(30);
+  }
+  else if(billValue > 200){
+    spendToken(20);
+  }
+}
+
 
 generate40RupeeToken(){
   developer.log("Generating 40 Rupee Token");
@@ -37,6 +74,24 @@ generate40RupeeToken(){
 generate20RupeeToken(){
   FirebaseFirestore firestore = FirebaseFirestore.instance;
   CollectionReference coins = firestore.collection("20RupeeToken");
+  // Specify your desired document name here
+  Random random = Random();
+  int randNum = random.nextInt(999999);
+  List<int> bytes = utf8.encode(randNum.toString());
+  var hash = sha256.convert(bytes);
+  // Add data to the specified document
+  coins.add({
+    'Hash': hash.toString(),
+    'UID': FirebaseAuth.instance.currentUser!.uid.toString(),
+    'multiplier': 1.0,
+    'source': 'Generated at $date',
+  });
+}
+
+
+generateHalfCoin(){
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  CollectionReference coins = firestore.collection("Half20");
   // Specify your desired document name here
   Random random = Random();
   int randNum = random.nextInt(999999);
